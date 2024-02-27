b043f0cf38f087b27560379eab8815ae33ccabd1
Habib KKML Dev <habibkkml@gmail.com>
2024-02-27 10:26:36 +0600

    Removed errors

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,27 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/pagination';
-import { EffectFade, Pagination, Autoplay } from 'swiper/modules';
+// import { EffectFade, Pagination, Autoplay } from 'swiper/modules';
 
 import './hero.scss';
 import Banner from '../Banner/Banner';
 
 const Hero = () => {
-    const heroData = [
-        {
-            img: 'https://res.cloudinary.com/dfaw271y6/image/upload/v1708407192/office_landscape_qllcxf.png',
-            alt: 'Khoshroz Group of Company',
-            title: 'Khoshroz Group of Company',
-            description: 'We are in publication business since 84 years'
-        },
-        {
-            img: 'https://res.cloudinary.com/dfaw271y6/image/upload/v1708407190/group_logo_uwfakr.png',
-            alt: 'Khoshroz Group of Company',
-            title: 'Khoshroz Group Limited',
-            description: 'We are in publication business since 84 years'
-        }
-    ];
+    // const heroData = [
+    //     {
+    //         img: 'https://res.cloudinary.com/dfaw271y6/image/upload/v1708407192/office_landscape_qllcxf.png',
+    //         alt: 'Khoshroz Group of Company',
+    //         title: 'Khoshroz Group of Company',
+    //         description: 'We are in publication business since 84 years'
+    //     },
+    //     {
+    //         img: 'https://res.cloudinary.com/dfaw271y6/image/upload/v1708407190/group_logo_uwfakr.png',
+    //         alt: 'Khoshroz Group of Company',
+    //         title: 'Khoshroz Group Limited',
+    //         description: 'We are in publication business since 84 years'
+    //     }
+    // ];
 
     return (
         <div className="container">
